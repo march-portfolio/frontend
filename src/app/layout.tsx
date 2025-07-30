d0eeac74d849de9fa6201f15784a76839bdabff1
march <march.creative27@gmail.com>
2025-07-31 00:20:33 +0700

    try css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,10 @@ export default function RootLayout({
   // let page = window.location.pathname === "/";
   return (
     <html lang="en">
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Thai:wght@100;200;300;400;500;600;700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
       <body className="layout">
         {/* <div>{page ? "" : <NavBar />}</div> */}
         <div><NavBar/></div>
