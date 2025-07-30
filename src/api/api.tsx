8e4e157b8683862d884f492941b0bb02dff14fea
march <march.creative27@gmail.com>
2025-07-31 02:56:35 +0700

    api

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const url = 'http://localhost:3000'
-const url2 = 'https://formsubmit.co/march.creative27@email.com'
+// const url2 = 'https://formsubmit.co/march.creative27@email.com'
 
 export async function TestAPI(body: unknown){
   try{
